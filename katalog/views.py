cfa2b1a97703f07a50550f4c0630ee0c6b7269d7
Laura Glory <laura.toruan@gmail.com>
2022-09-15 12:43:24 +0700

    tugas2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from katalog.models import CatalogItem
+
+from django.shortcuts import render
+
+
 # TODO: Create your views here.
+def show_katalog(request):
+    
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog': data_katalog,
+        'nama': 'Laura'
+    }
+    return render(request, "katalog.html", context)
